d72e4c4e3febfb467221f3393d6ca6c6b5871e2a
Asma'a <78433466+asmaacode@users.noreply.github.com>
2022-10-12 23:34:37 +0300

    Update #13 CompareDates.cpp

--- #13 CompareDates.cpp
+++ #13 CompareDates.cpp
@@ -3,23 +3,21 @@
 using namespace std;
 using namespace myLibrary;
 
-bool compareDates(calendar::sDate date1, calendar::sDate date2) {
-	if (date1.year > date2.year) return true;
-	if (date1.month > date2.month) return true;
-	if (date1.day > date2.day) return true;
-
-	return false;
+bool isDate1LessThanDate2(calendar::sDate date1, calendar::sDate date2) {
+	return (date1.year < date2.year) ? true :
+			(date1.year == date2.year) ? (date1.month < date2.month) ? true :
+			(date1.month == date2.month) ? (date1.day < date2.day)  : false: false;
 }
 int main() {
 	cout << "Date1: \n";
-	calendar::sDate date1= read::readDate();
+	calendar::sDate date1 = read::readDate();
 	cout << "Date2: \n";
-	calendar::sDate date2= read::readDate();
-	
-	if (compareDates(date1, date2))
-		cout << "No, Date1 is greater than Date2";
-	else
+	calendar::sDate date2 = read::readDate();
+
+	if (isDate1LessThanDate2(date1, date2))
 		cout << "Yes, Date1 is less than Date2";
+	else
+		cout << "No, Date1 is greater than Date2";
 
 
 	return 0;
