dfd95f1b30387738d5e4f94c48e3783b1a459a4a
Asma'a <78433466+asmaacode@users.noreply.github.com>
2022-10-14 22:36:19 +0300

    Update #17 DiffInDays.cpp

--- #17 DiffInDays.cpp
+++ #17 DiffInDays.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 using namespace myLibrary;
 
-int getDiffrenceDays(calendar::sDate  date1,calendar::sDate date2,bool includingEndDay=false) {
+int getDifferenceDays(calendar::sDate  date1,calendar::sDate date2,bool includingEndDay=false) {
 
 	int counter = (includingEndDay ? 1 : 0);
 	while (calendar::isDate1LessThanDate2(date1, date2)) {
@@ -20,9 +20,9 @@ int main() {
 	cout << "Date2 :\n";
 	calendar::sDate date2 = read::readDate();
 
-	cout << "Diffrence is " << getDiffrenceDays(date1,date2) << " Day(s).";
+	cout << "Difference is " << getDifferenceDays(date1,date2) << " Day(s).";
 	cout << "\n\n";
-	cout << "Diffrence is (Including End Day) " << getDiffrenceDays(date1,date2,true)<<" Day(s).";
+	cout << "Difference is (Including End Day) " << getDifferenceDays(date1,date2,true)<<" Day(s).";
 	cout << "\n\n";
 	return 0;
 }
